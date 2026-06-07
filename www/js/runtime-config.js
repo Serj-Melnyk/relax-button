001addb96e6bd9eaa80ca82ee86fb9d1807e00e7
Serj-Melnyk <103459330+Serj-Melnyk@users.noreply.github.com>
2026-06-07 16:57:25 +0300

    Add runtime config scaffold

---
+++ www/js/runtime-config.js
@@ -0,0 +1,14 @@
+window.__APP_RUNTIME_CONFIG__ = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  billing: {
+    premiumProductId: "premium_lifetime",
+    receiptValidationUrl: ""
+  }
+};
