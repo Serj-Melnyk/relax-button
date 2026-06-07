c2952d9a441984616c524fcd8033c133ed5abbb4
Serj-Melnyk <103459330+Serj-Melnyk@users.noreply.github.com>
2026-06-07 10:36:15 +0300

    Add runtime config example

---
+++ www/js/runtime-config.example.js
@@ -0,0 +1,14 @@
+window.__APP_RUNTIME_CONFIG__ = {
+  firebase: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  },
+  billing: {
+    premiumProductId: "premium_lifetime",
+    receiptValidationUrl: "https://us-central1-YOUR_PROJECT_ID.cloudfunctions.net/validateReceipt"
+  }
+};
